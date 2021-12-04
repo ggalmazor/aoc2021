6be13024f51c56425e266992425bc3fb0210b9f9
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2021-12-04 12:40:28 +0100

    Remove redundant value copy

--- nodejs/3/solution.mjs
+++ nodejs/3/solution.mjs
@@ -7,7 +7,7 @@ const lines = readLines(import.meta.url)
 
 
 function filterLines(lines, filterMostFrequent = true) {
-  let candidateLines = JSON.parse(JSON.stringify(lines));
+  let candidateLines = lines;
   let bitIndex = 0;
   while (candidateLines.length > 1 && bitIndex < 12) {
     let bitBias = candidateLines.map(line => line[bitIndex] === '1' ? 1 : -1).reduce(sum, 0);
@@ -24,7 +24,7 @@ function part1(lines) {
   const gammaRate = parseInt(lines
     .reduce((counts, line) => zip(counts, line.split('')
       .map(v => v === '0' ? -1 : 1))
-      .map(([a, b]) => a + b), [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+      .map(pair=> sum(...pair)), [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     .map(count => count > 0 ? '1' : '0')
     .join(''), 2);
   const epsilonRate = bitwiseComplement(gammaRate);
