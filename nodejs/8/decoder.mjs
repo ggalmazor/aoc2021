c9575f8e7b201572f5bd27df243cfe13955ab600
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2021-12-08 11:48:06 +0100

    Fix comment

--- nodejs/8/decoder.mjs
+++ nodejs/8/decoder.mjs
@@ -4,22 +4,22 @@ import {diff} from "../lib/strings.mjs";
 
 /**
  0:      1:      2:      3:      4:
- aaaa    ....    aaaa    aaaa    ....
+  aaaa    ....    aaaa    aaaa    ....
  b    c  .    c  .    c  .    c  b    c
  b    c  .    c  .    c  .    c  b    c
- ....    ....    dddd    dddd    dddd
+  ....    ....    dddd    dddd    dddd
  e    f  .    f  e    .  .    f  .    f
  e    f  .    f  e    .  .    f  .    f
- gggg    ....    gggg    gggg    ....
+  gggg    ....    gggg    gggg    ....
 
  5:      6:      7:      8:      9:
- aaaa    aaaa    aaaa    aaaa    aaaa
+  aaaa    aaaa    aaaa    aaaa    aaaa
  b    .  b    .  .    c  b    c  b    c
  b    .  b    .  .    c  b    c  b    c
- dddd    dddd    ....    dddd    dddd
+  dddd    dddd    ....    dddd    dddd
  .    f  e    f  .    f  e    f  .    f
  .    f  e    f  .    f  e    f  .    f
- gggg    gggg    ....    gggg    gggg
+  gggg    gggg    ....    gggg    gggg
  */
 const segmentTranslations = {
   'abcefg': 0,
